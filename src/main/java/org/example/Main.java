161cdbe5256c2dae969f3c868141a692499e859d
Alberto Rosas 🇵🇦 <97554566+arosaspa@users.noreply.github.com>
2025-06-16 22:19:45 -0500

    Update Main.java
    First Part

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,21 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String message = "**************************************" +
+                         "\n* Welcome to Yuan Currency Converter *" +
+                         "\n**************************************";
+        System.out.println(message);
+
+        String list = "--------------------------------------" +
+                "\n* Please select an option: *" +
+                      "\n* 1. Convert dollar $ to ¥ Yuan " +
+                      "\n* 2. Convert Euro E to ¥ Yuan" +
+                      "\n* 3. Bitcoin B to ¥ Yuan" +
+                      "\n* 3. Exit" +
+                      "\n -------------------------------------";
+
+
+        System.out.println(list);
     }
 }
